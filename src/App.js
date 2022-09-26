02f2afef58c61032ccefe32c3d1fef546b0a7250
mattlunzer <matt.lunzer@gmail.com>
2022-09-26 17:31:07 -0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Hello world!</h1>
         </header>
         <p className="App-intro">
-          Looks like you're all set!
+          Looks like you're all set v2!
         </p>
       </div>
     );
